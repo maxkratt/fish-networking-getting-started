df5f2f3a9117ba59088e423a97e382c88258431d
Max Kratt <maxkratt@gmail.com>
2025-06-17 20:38:20 +0200

    make player control scene camera

--- Assets/PlayerCamera.cs
+++ Assets/PlayerCamera.cs
@@ -1,19 +1,27 @@
+using FishNet.Connection;
 using FishNet.Object;
 using UnityEngine;
 
 // This script will be a NetworkBehaviour so that we can use the 
-// OnStartClient override.
+// OnOwnershipClient override.
 public class PlayerCamera : NetworkBehaviour
 {
-    [SerializeField] private Camera cameraPrefab;
     [SerializeField] private Transform cameraHolder;
 
-    // This method will run on the client once this object is spawned.
-    public override void OnStartClient()
+    // This method is called on the client after gaining or losing ownership of the object.
+    // We could have used OnStartClient instead, as we did in the previous example, but using OnOwnershipClient
+    // means this will work for a player object we don't initially own but are given ownership to later.
+    public override void OnOwnershipClient(NetworkConnection prevOwner)
     {
-        // Since this will run on all clients that this object spawns for
-        // we need to only instantiate the camera for the object we own.
+        if (Camera.main == null)
+            return;
+
+        // If we are the new owner of this object, then take control of the camera by parenting it
+        // and moving it to our camera holder.
         if (IsOwner)
-            Instantiate(cameraPrefab, cameraHolder.position, cameraHolder.rotation, cameraHolder);
+        {
+            Camera.main.transform.SetPositionAndRotation(cameraHolder.position, cameraHolder.rotation);
+            Camera.main.transform.SetParent(cameraHolder);
+        }
     }
 }
